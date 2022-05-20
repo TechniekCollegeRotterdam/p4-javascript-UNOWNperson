ac99582b5a7bb3c74e56d72f708e388c4970f1cb
Maik Alexander Lüdeck <9016083@student.tcrmbo.nl>
2022-05-20 14:07:08 +0200

    zashu

--- les2/app.js
+++ les2/app.js
@@ -12,3 +12,30 @@ bird.addEventListener("click", function() {
 })
 
 
+window.addEventListener("keydown", function(e) {
+    
+  console.log(e.key);
+
+
+  if(e.key == "ArrowRight"){ 
+   px = px + 50;
+   bird.style.left = px + "px";
+  }
+  
+  if(e.key == "ArrowLeft"){ 
+    px = px - 50;
+    bird.style.left = px + "px";
+   }
+
+   if(e.key == "ArrowUp"){ 
+    px = px + 50;
+    bird.style.bottom = px + "px";
+   }
+
+   if(e.key == "ArrowDown"){ 
+    px = px - 50;
+    bird.style.bottom = px + "px";
+   }
+
+
+})
